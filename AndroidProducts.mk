c03478fdfd5b1cd899257bdc058dec650d28b773
yonekome <117240823+yonekome@users.noreply.github.com>
2026-01-29 15:24:24 +0900

    Update product makefiles and lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_nabu.mk
+    $(LOCAL_DIR)/infinity_nabu.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_nabu-user \
-    lineage_nabu-userdebug \
-    lineage_nabu-eng
+    infinity_nabu-user \
+    infinity_nabu-userdebug \
+    infinity_nabu-eng
