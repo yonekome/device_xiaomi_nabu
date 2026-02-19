d5d7a360aa7424678bc7a605dc44aaaf42d585c6
yonekome <117240823+yonekome@users.noreply.github.com>
2026-02-19 17:50:59 +0900

    Inherit common Infinity X configurations

--- infinity_nabu.mk
+++ infinity_nabu.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Inherit some common Infinity X stuff.
+$(call inherit-product, vendor/infinity/config/common_full_tablet_wifionly.mk)
+
 # Inherit some common AOSP stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
